bdb4fc01e21537ffcccb39fec405fbbe92f13ed2
barts <bartsteensma@outlook.com>
2024-05-08 09:28:48 +0200

    fix: abbreviations must only be expanded in command mode :

--- lua/projectmarks/abbreviations.lua
+++ lua/projectmarks/abbreviations.lua
@@ -1,11 +1,20 @@
 local M = {}
 
+M.colon_abbrev = function(keyword, expansion)
+  if vim.fn.getcmdtype() == ':' and vim.fn.getcmdline() == keyword then
+    return expansion
+  end
+  return keyword
+end
+
 --- Setup the abbreviations for the plugin.
 ---@param opts Options The options provided by the user.
 M.setup = function(opts)
   if opts.abbreviations then
-    vim.cmd("cnoreabbrev mark Mark")
-    vim.cmd("cnoreabbrev delmarks DelMarks")
+    local mark = [[luaeval("require('projectmarks.abbreviations').colon_abbrev('mark', 'Mark')")]]
+    local delmarks = [[luaeval("require('projectmarks.abbreviations').colon_abbrev('delmarks', 'DelMarks')")]]
+    vim.api.nvim_set_keymap("ca", "mark", mark, { expr = true })
+    vim.api.nvim_set_keymap("ca", "delmarks", delmarks, { expr = true })
   end
 end
 
